372e8040f4bd7c5f512bdfd7a316c8913c04941f
bandithedoge <bandithedoge@protonmail.com>
2023-08-19 02:14:06 +0200

    use flake-parts

--- flake.nix
+++ flake.nix
@@ -1,59 +1,43 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = {
+  outputs = inputs @ {
     self,
-    nixpkgs,
-    flake-utils,
+    flake-parts,
+    ...
   }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {inherit system;};
-        drv = hostPkgs:
-          hostPkgs.stdenv.mkDerivation {
-            pname = "Misstrhortion";
-            version = "1.0.0";
-            src = ./.;
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "x86_64-darwin" "aarch64-darwin"];
+      imports = [inputs.flake-parts.flakeModules.easyOverlay];
+      perSystem = {
+        config,
+        pkgs,
+        ...
+      }: rec {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "Misstrhortion";
+          version = "1.0.0";
+          src = ./.;
 
-            nativeBuildInputs = with hostPkgs; [
-              cmake
-              pkg-config
-            ];
+          nativeBuildInputs = with pkgs; [
+            cmake
+            pkg-config
+          ];
 
-            buildInputs = with hostPkgs;
-              lib.optionals stdenv.hostPlatform.isLinux [
-                alsa-lib
-                dbus
-                libGL
-                xorg.libX11
-                xorg.libXScrnSaver
-                xorg.libXcursor
-                xorg.libXext
-                xorg.libXfixes
-                xorg.libXi
-                xorg.libXrandr
-              ]
-              ++ (lib.optionals stdenv.hostPlatform.isMusl [libexecinfo]);
-          };
-      in rec {
-        packages = rec {
-          inherit (misstrhortion) default;
-          misstrhortion = {
-            default = drv pkgs;
-            linux64 = drv pkgs.pkgsCross.musl64;
-            # linux64 = drv (import nixpkgs {
-            #   inherit system;
-            #   crossSystem = nixpkgs.lib.systems.examples.musl64;
-            # });
-          };
+          buildInputs = with pkgs;
+            lib.optionals stdenv.hostPlatform.isLinux [
+              libGL
+              xorg.libX11
+            ]
+            ++ (lib.optionals stdenv.hostPlatform.isMusl [libexecinfo]);
         };
         devShells.default = pkgs.stdenv.mkDerivation {
           name = packages.default.pname;
           inherit (packages.default) buildInputs;
           nativeBuildInputs = packages.default.nativeBuildInputs ++ (with pkgs; [clang-tools]);
         };
-      }
-    );
+      };
+    };
 }
